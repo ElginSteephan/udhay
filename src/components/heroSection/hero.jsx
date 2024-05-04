59395708441aa6b30bcb9724fc98fa41e1f1bb2e
ELGIN STEEPHAN THOMAS <95463699+ElginSteephan@users.noreply.github.com>
2024-05-04 14:45:01 +0530

    Update hero.jsx

--- src/components/heroSection/hero.jsx
+++ src/components/heroSection/hero.jsx
@@ -2,19 +2,7 @@ import React, { useState, useRef } from "react";
 import "./hero.css";
 
 function HeroBG() {
-  const [showPopup, setShowPopup] = useState(false); // State to control popup visibility
-  const popupRef = useRef(null); // Ref for the popup
-
-  const handleClick = () => {
-    setShowPopup(true); // Set showPopup state to true when button is clicked
-    // Smooth scroll to the section where the popup is located
-    popupRef.current.scrollIntoView({ behavior: "smooth" });
-  };
-
-  const handleClose = () => {
-    setShowPopup(false); // Set showPopup state to false to close the popup
-    popupRef.current.scrollIntoView({ behavior: "smooth" });
-  };
+  
 
   return (
     <>
