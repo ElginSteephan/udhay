ac6c55f87a3d072a4bc8113ce8791ba69924e694
ELGIN STEEPHAN THOMAS <95463699+ElginSteephan@users.noreply.github.com>
2024-09-26 16:11:56 +0530

    Update hero.jsx

--- src/components/heroSection/hero.jsx
+++ src/components/heroSection/hero.jsx
@@ -8,7 +8,7 @@ function HeroBG() {
     <>
       <div className="BG">
         <h1>
-          35+Years of Expertise to Simplify Your Procedures </h1><h2> Expedite Shipments Worldwide
+          40+Years of Expertise to Simplify Your Procedures </h1><h2> Expedite Shipments Worldwide
         </h2>
         <h3>Streamline Your Customs Procedures with <span><h3>UDHAY SHIPPING</h3></span></h3>
        
